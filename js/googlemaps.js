edb3912dc59e23ec220e120e191fabfc5eaabca5
KJ Johnson <kj@fantasyteamadvice.com>
2017-09-12 15:36:11 -0600

    feat: add markers array and code
    Create a markers array and then add the function to generate
    markers which will appear on the map

--- js/googlemaps.js
+++ js/googlemaps.js
@@ -1,8 +1,30 @@
 var map;
+// Create empty array variable to store markers in
+var markers = [];
 function initMap() {
-    // Constructor creates a new map - only center and zoom are required.
+    // Create a new map with zoom auto-set to 14
     map = new google.maps.Map(document.getElementById('map'), {
       center: {lat: 33.812092, lng: -117.918974},
-      zoom: 13
+      zoom: 14
       });
+      var locations = [
+          {title: 'Disneyland', location: {lat: 33.812092, lng: -117.918974}},
+          {title: 'California Adventure', location: {lat: 33.804220, lng: -117.920859}},
+          {title: 'Angel Stadium', location: {lat: 33.800308, lng: -117.882732}},
+          {title: 'Anaheim Convention Center', location: {lat: 33.800672, lng: -117.920873}},
+          {title: 'Downtown Disney', location: {lat: 33.809209, lng: -117.923157}},
+        ];
+        // iterate over locations array returning markers
+        for (var i = 0; i < locations.length; i++){
+          var position = locations[i].location;
+          var title = locations[i].title;
+          var marker = new google.maps.Marker({
+            position: position,
+            animation: google.maps.Animation.DROP,
+            title: title,
+            map: map,
+          });
+          // Push each marker to our empty markers array
+          markers.push(marker);
+        }
     }
