ef1b40d05c772b746ae50a14176b6f370679ba92
KJ Johnson <kj@fantasyteamadvice.com>
2017-09-16 12:26:26 -0600

    feat: move foursquare id and secret
    Declared the foursquare clientID and secret inside the viewmodel

--- js/app.js
+++ js/app.js
@@ -1,65 +1,62 @@
+//date to use with Foursquare api calls
+var foursquaredate;
+var clientID;
+var clientSecret;
 var map;
-var initialMarkers = [
-    {title: 'Disneyland', location: {lat: 33.812092, lng: -117.918974}},
-    {title: 'California Adventure', location: {lat: 33.804220, lng: -117.920859}},
-    {title: 'Angel Stadium', location: {lat: 33.800308, lng: -117.882732}},
-    {title: 'Anaheim Convention Center', location: {lat: 33.800672, lng: -117.920873}},
-    {title: 'Downtown Disney', location: {lat: 33.809209, lng: -117.923157}},
-  ];
-// Create empty array variable to store markers in
-var markers = [];
-var Marker = function(data) {
-  this.title = ko.observable(data.title);
-  this.location = ko.observable(data.location);
-}
+
+var initialData = {
+    filters: ["Choose a destination", "House of Mouse", "Theme Park #2", "The Big A", "Meetup central", "Outdoor shopping mall"],
+    markers: [
+      {title: 'Disneyland', location: {lat: 33.812092, lng: -117.918974}, icon: 'http://maps.google.com/mapfiles/ms/icons/green-dot.png', nick: "House of Mouse"},
+      {title: 'California Adventure', location: {lat: 33.804220, lng: -117.920859}, icon: 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png', nick: "Theme Park #2"},
+      {title: 'Angel Stadium', location: {lat: 33.800308, lng: -117.882732}, icon: 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png', nick: "The Big A"},
+      {title: 'Anaheim Convention Center', location: {lat: 33.800672, lng: -117.920873}, icon: 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png', nick: "Meetup central"},
+      {title: 'Downtown Disney', location: {lat: 33.809209, lng: -117.923157}, icon: 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png', nick: "Outdoor shopping mall"}  ]
+};
+
 
 function initMap() {
-    // Create a new map with zoom auto-set to 14
+    var self = this;
+    // Create a new map with zoom auto-set to 13
     map = new google.maps.Map(document.getElementById('map'), {
       center: {lat: 33.812092, lng: -117.918974},
       zoom: 13
       });
-
         // iterate over locations array returning markers
-        for (var i = 0; i < initialMarkers.length; i++){
-          var position = initialMarkers[i].location;
-          var title = initialMarkers[i].title;
+        for (var i = 0; i < 5; i++){
+          var position = initialData.markers[i].location;
+          var title = initialData.markers[i].title;
+          var icon = initialData.markers[i].icon;
           var marker = new google.maps.Marker({
             position: position,
             animation: google.maps.Animation.DROP,
             title: title,
             map: map,
-            icon: 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png'
+            icon: icon
           });
-          // Push each marker to our empty markers array
-          markers.push(marker);
         }
     }
 
-var ViewModel = function() {
-  var self = this;
-  // Create markerList to display on menu
-  this.markerList = ko.observableArray([]);
-  // For each marker push it to the markerList array
-  initialMarkers.forEach(function(thisMarker){
-        self.markerList.push(new Marker(thisMarker));
-        });
-  // Math logic to help set currentMarker to a random marker
-  this.getRandomMarker = function(min, max) {
-      return Math.floor(Math.random() * (max - min + 1)) + min;
-  };
-  // Set currentMarker to random marker on load
-  self.currentMarker = ko.observable( self.markerList()[self.getRandomMarker(0, 4)] );
 
-  // Set map name
-  this.name=ko.observable('Anaheim');
-
-  // Set currentMarker to the button that was clicked
-  this.setCurrentMarker = function(clickedMarker) {
-    self.currentMarker(clickedMarker);
-  };
-
-};
-
-
-ko.applyBindings(new ViewModel())
+var ViewModel = function(data) {
+    var self = this;
+    // Store clientID and secret for foursquare API
+    clientID = "JTNSWR0O4211C3F5BO0NP1SBLEQR0FH2APFYYPWLXD1OPFLD";
+    clientSecret = "KEZMBI3TX1EK5NDBUTGAWM1L3NOX554HQPZOTOTIQDLTKQPF";
+    foursquaredate = 20170915;
+    this.name = ko.observable('Map of Anaheim');
+    self.filters = ko.observableArray(data.filters);
+    self.filter = ko.observable('');
+    self.markers = ko.observableArray(data.markers);
+    self.filteredMarkers = ko.computed(function() {
+        var filter = self.filter();
+        if (!filter || filter == "Choose a destination") {
+            return self.markers();
+        } else {
+            return ko.utils.arrayFilter(self.markers(), function(i) {
+                return i.nick == filter;
+                       });
+                   }
+               });
+             }
+ko.applyBindings(new ViewModel(initialData));
