bf7cf705fa60b217f239c27c3a58c50b4e3273b3
KJ Johnson <kj@fantasyteamadvice.com>
2017-09-19 08:12:07 -0600

    feat: handle errors for google maps
    If maps fails to load display error message after 1.5 seconds

--- js/app.js
+++ js/app.js
@@ -3,15 +3,35 @@ var foursquaredate;
 var clientID;
 var clientSecret;
 var map;
+var $map = $('#map')
 
 var initialData = {
     filters: ["Choose a destination", "Disneyland", "California Adventure", "Angel Stadium", "Anaheim Convention Center", "Downtown Disney"],
     markers: [
-      {title: 'Disneyland', location: {lat: 33.812092, lng: -117.918974}, icon: 'http://maps.google.com/mapfiles/ms/icons/green-dot.png'},
-      {title: 'California Adventure', location: {lat: 33.804220, lng: -117.920859}, icon: 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png'},
-      {title: 'Angel Stadium', location: {lat: 33.800308, lng: -117.882732}, icon: 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png'},
-      {title: 'Anaheim Convention Center', location: {lat: 33.800672, lng: -117.920873}, icon: 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png'},
-      {title: 'Downtown Disney', location: {lat: 33.809209, lng: -117.923157}, icon: 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png'}  ]
+      {
+        title: 'Disneyland',
+        location: {lat: 33.812092, lng: -117.918974},
+        icon: 'http://maps.google.com/mapfiles/ms/icons/green-dot.png'
+      },
+      {
+        title: 'California Adventure',
+        location: {lat: 33.804220, lng: -117.920859},
+        icon: 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png'
+      },
+      {
+        title: 'Angel Stadium',
+        location: {lat: 33.800308, lng: -117.882732},
+        icon: 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png'
+      },
+      {
+        title: 'Anaheim Convention Center',
+        location: {lat: 33.800672, lng: -117.920873},
+        icon: 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png'
+      },
+      {
+        title: 'Downtown Disney',
+        location: {lat: 33.809209, lng: -117.923157},
+        icon: 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png'}  ]
 };
 
 var foursquareInfo = function(data) {
@@ -25,7 +45,7 @@ function startMap() {
     map = new google.maps.Map(document.getElementById('map'), {
       center: {lat: 33.812092, lng: -117.918974},
       zoom: 13
-      });
+    });
         // iterate over locations array returning markers
         for (var i = 0; i < 5; i++){
           var position = initialData.markers[i].location;
@@ -48,6 +68,10 @@ var ViewModel = function(data) {
     clientID = "JTNSWR0O4211C3F5BO0NP1SBLEQR0FH2APFYYPWLXD1OPFLD";
     clientSecret = "KEZMBI3TX1EK5NDBUTGAWM1L3NOX554HQPZOTOTIQDLTKQPF";
     foursquaredate = 20170915;
+    // Set timeout error message in case map fails to load
+    setTimeout(function(){
+        $map.append('<h2>' + "Failed to load map. Please wait a few seconds and try again" + '</h2>');
+    }, 1500);
     this.name = ko.observable('Map of Anaheim');
     self.filters = ko.observableArray(data.filters);
     self.filter = ko.observable('');
